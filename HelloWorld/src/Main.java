729328f6a7f5bb2d713387cee5ab4ea0c19fe7d2
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-17 08:12:47 +0300

    Basics

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,6 +2,57 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+//
+//        boolean isAlien = false;
+//
+//        //Conditionals
+//        if(isAlien){
+//            System.out.println("Alien");
+//        }
+//        else {
+//            System.out.println("Not Alien");
+//        }
+//
+//        int topScore = 100;
+//
+//        if(topScore>=100){
+//            System.out.println("Top score is greater than 100");
+//        }
+//
+//        int secondTopScore = 200;
+//
+//        if(secondTopScore>topScore && secondTopScore<300){
+//            System.out.println("Second top score is less than 300");
+//        }
+//        if(secondTopScore<topScore || secondTopScore<300){
+//            System.out.println("Second top score is less than 300");
+//        }
+//
+//        //Ternary Operator
+//        String makeOfCar = "Volkswagen";
+//        boolean isDomestic = makeOfCar == "Mercedes-Benz" ? true : false;
+//        if(isDomestic){
+//            System.out.println("Domestic");
+//        }
+
+        double first = 20.00d;
+
+        double second = 80.00d;
+
+        double sum = (first + second) * 100;
+
+        double remainder = sum % 40.00d;
+
+        boolean isValid = remainder == 0.00d ? true : false;
+
+        System.out.println(isValid);
+
+        if (!isValid) {
+            System.out.println("got some remainder");
+        }
+
+
+
     }
 }
