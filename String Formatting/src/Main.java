0e2d26ac6f5f7c3a0e38ea62a24446c0b82e97e1
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-18 19:06:16 +0300

    Text block triple double quotes

---
+++ String Formatting/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       String bulletIt = "Print a Bulleted List: \n" +
+               "\t \u2022 First Point\n" +
+               "\t\t\u2022 Sub Point";
+       System.out.println(bulletIt);
+
+       String textBlock = """
+              Print a Bulleted List:
+                    \u2022 First Point
+                        \u2022 Sub Point""";
+       System.out.println(textBlock);
+
+
+    }
+}
