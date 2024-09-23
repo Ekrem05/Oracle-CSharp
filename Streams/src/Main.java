9cda0dbf5a033bdd96664f61685223275ee94e73
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-23 08:05:53 +0300

    Stream

---
+++ Streams/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<Integer>(List.of(2,3,1,3,33,124,56,47,888,554,24,67));
+
+        list.stream()
+                .filter(x->x%2==0)
+                .distinct()
+                .forEach(System.out::println);
+        
+        //Basically you can perform operations on a collection functionally
+    }
+}
