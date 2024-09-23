fbb78afa7e22f76962b48c60bf16eefc59e91018
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-23 07:55:17 +0300

    Maps

--- Sets and Maps/src/Main.java
+++ Sets and Maps/src/Main.java
@@ -1,7 +1,4 @@
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -10,13 +7,13 @@ public class Main {
 
         List<Contact> emails = ContactData.getData("email");
         List<Contact> phones = ContactData.getData("phone");
-        printCollection("Emails",emails);
-        printCollection("Phones",phones);
+        Map<String,Contact> map = new HashMap<>();
 
-        Set<Contact> emailSet = new HashSet<>(emails);
-        Set<Contact> phoneSet = new HashSet<>(phones);
-        printCollection("Emails set",emailSet);
-        printCollection("Phones set",phoneSet);
+       for (Contact c : emails) {
+           map.put(c.getName(),c);
+       }
+        map.forEach((k,v)->System.out.printf("%s -> %s \n",k,v));
+       Set<String> keys = map.keySet();
     }
 
     public static void printCollection(String header, Collection<Contact> contacts){
