e6d9b5f6c99a8bf1e3fe92660946c76df8444548
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-24 17:16:19 +0300

    Sql injection, parameterized queries

--- JDBC/src/Main.java
+++ JDBC/src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,26 +10,34 @@ public class Main {
         try(Connection conn = DriverManager.getConnection(CONN_STRING,"root","Admin1234")){
             System.out.println("Connected to database");
 
-            PreparedStatement stm = conn.prepareStatement("SELECT * FROM music.songs");
-            ResultSet result = stm.executeQuery();
-            for(int i = 1; i<result.getMetaData().getColumnCount()+1; i++){
-                System.out.printf("%-25s ",result.getMetaData().getColumnLabel(i));
-            }
-            System.out.println();
-            while(result.next()){
+            System.out.print("Enter song id: ");
+            Scanner scanner = new Scanner(System.in);
+            String id = scanner.nextLine();
 
-                for(int i = 1; i<result.getMetaData().getColumnCount()+1; i++){
-                    System.out.printf("%-25s ",result.getString(i));
-                }
-                System.out.println();
-//                int id = result.getInt("song_id");
-//                int track_number = result.getInt("track_number");
-//                String title = result.getString("song_title");
-//                System.out.printf("ID: %d     Track Number: %d     Title: %s\n", id, track_number, title);
-            }
+            PreparedStatement stm = conn.prepareStatement("SELECT * FROM music.songs WHERE song_id = ?");
+            stm.setString(1, id);
+            ResultSet result = stm.executeQuery();
+            printTable(result);
+
+
+
         }
         catch (SQLException e){
             e.printStackTrace();
+        }
+    }
+    public static void printTable(ResultSet result) throws SQLException {
+        System.out.println("==============================================");
+        for(int i = 1; i<result.getMetaData().getColumnCount()+1; i++){
+            System.out.printf("%-25s ",result.getMetaData().getColumnLabel(i));
+        }
+        System.out.println();
+        while(result.next()){
+
+            for(int i = 1; i<result.getMetaData().getColumnCount()+1; i++){
+                System.out.printf("%-25s ",result.getString(i));
+            }
+            System.out.println();
         }
     }
 }
