573ecd105dbcb5b26981364fb7688976f0fe06d0
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-09-24 17:41:46 +0300

    Insert

--- JDBC/src/Main.java
+++ JDBC/src/Main.java
@@ -10,14 +10,43 @@ public class Main {
         try(Connection conn = DriverManager.getConnection(CONN_STRING,"root","Admin1234")){
             System.out.println("Connected to database");
 
-            System.out.print("Enter song id: ");
-            Scanner scanner = new Scanner(System.in);
-            String id = scanner.nextLine();
+            System.out.print("""
+                    Select an action:
+                    1. Print songs
+                    2. Add song
+                    3. Delete song
+                    """);
+            Scanner sc = new Scanner(System.in);
+            Integer action = sc.nextInt();
+            if(action == 1){
+                System.out.print("Enter song id: ");
+                Scanner scanner = new Scanner(System.in);
+                String id = scanner.nextLine();
 
-            PreparedStatement stm = conn.prepareStatement("SELECT * FROM music.songs WHERE song_id = ?");
-            stm.setString(1, id);
-            ResultSet result = stm.executeQuery();
-            printTable(result);
+                PreparedStatement stm = conn.prepareStatement("SELECT * FROM music.songs WHERE song_id = ?");
+                stm.setString(1, id);
+                ResultSet result = stm.executeQuery();
+                printTable(result);
+            }
+            else if(action == 2){
+                System.out.print("Enter track number: ");
+                Scanner scanner = new Scanner(System.in);
+                int track = scanner.nextInt();
+                System.out.print("Enter song title: ");
+                Scanner titlescanner = new Scanner(System.in);
+                String title = titlescanner.nextLine();
+                System.out.print("Enter album id: ");
+                Scanner albumscanner = new Scanner(System.in);
+                int albumId = albumscanner.nextInt();
+                 PreparedStatement stm = conn.prepareStatement("Insert into music.tracks(track_number, song_title,album_id) values(?,?,?)");
+                 stm.setInt(1, track);
+                 stm.setString(2, title);
+                 stm.setInt(3, albumId);
+                int rowsAffected = stm.executeUpdate();
+                System.out.println(rowsAffected + " row(s) inserted.");
+
+            }
+
 
 
 
